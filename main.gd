8301d8d6a3da41c5d2572ee6a8a7c13074c82e27
Kazzak22 <jakopsskeleton@gmail.com>
2024-06-03 11:01:20 -0600

    actualizacion del sistema

--- main.gd
+++ main.gd
@@ -1,16 +1,31 @@
 extends Node2D
-
+@export var mob_scence: PackedScene
 var screen_size : Vector2i
 
 var PlayerInitialPosition
 
 func  _ready():
 	screen_size = get_window().size
-	PlayerInitialPosition = $Player .position
-	print(PlayerInitialPosition)
+	PlayerInitialPosition = $Player.position
+	GHud.start_game.connect(new_game)
+	
+func game_over():
+	GHud.update_high_score_label()
+	
 func new_game():
+	print('empieza')
 	$Floor.position.x = 0
 	$Player.position = PlayerInitialPosition
+	$MOBTimer.start() 
 
 func _process (delta):
-	$Floor.position.x = $Player.position.x - 250
+	$Floor.position.x = $Player.position.x - 150
+
+
+func _on_mob_timer_timeout():
+	var mob = mob_scence.instantiate()
+	mob.position.x = $Player.position.x + 1500
+	mob.position.y = 550
+	add_child(mob)
+	mob.hit.connect(game_over)
+	$MOBTimer.wait_time = 2
--- mob/mob.gd
+++ mob/mob.gd
@@ -4,24 +4,26 @@ signal hit
 
 func _ready():
 	var collisions = [
-		$SquareA,
+		$SquareC,
 	] 
 	var sprites = [
-		$SquareA,
+		$SquareS,
 	]
 	var random_int = randi() % collisions.size()
-	var selected_collisions = collisions [random_int]
+	var selected_collision = collisions [random_int]
 	var selected_sprite = sprites[random_int]
-	selected_collisions.disabled = false
-	selected_sprite.visible = true
 
+	selected_collision.disabled = false
+	selected_collision.visible = true
+	selected_sprite.visible = true
+	
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
 
 
 func _on_body_entered(body):
-	if body. is_in_proup("dinosaurio"):
+	if body.is_in_proup("dinosaurio"):
 		print("game over")
 		hit.emit()
 
